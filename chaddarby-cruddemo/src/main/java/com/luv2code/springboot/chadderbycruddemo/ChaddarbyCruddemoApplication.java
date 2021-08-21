653870dba855ee8c53102892efc815959cceab35
PierreSQS <nomo_messina@web.de>
2021-08-22 01:05:12 +0200

    initial Setup chaddarby-cruddemo

---
+++ chaddarby-cruddemo/src/main/java/com/luv2code/springboot/chadderbycruddemo/ChaddarbyCruddemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.chadderbycruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaddarbyCruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaddarbyCruddemoApplication.class, args);
+	}
+
+}
---
+++ chaddarby-cruddemo/src/test/java/com/luv2code/springboot/chadderbycruddemo/ChaddarbyCruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.chadderbycruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaddarbyCruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
