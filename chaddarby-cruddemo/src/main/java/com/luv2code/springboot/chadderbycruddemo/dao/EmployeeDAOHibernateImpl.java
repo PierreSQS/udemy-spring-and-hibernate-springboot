ee1bb9283ca38d64e2172ed4bd028f1af8c391b8
PierreSQS <nomo_messina@web.de>
2021-08-22 10:18:03 +0200

    Chap534: added additional CRUD-Methods in DAO

--- chaddarby-cruddemo/src/main/java/com/luv2code/springboot/chadderbycruddemo/dao/EmployeeDAOHibernateImpl.java
+++ chaddarby-cruddemo/src/main/java/com/luv2code/springboot/chadderbycruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class EmployeeDAOHibernateImpl implements EmployeeDAO{
@@ -26,5 +27,39 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO{
 
         // get the result list and return it
         return employeesQuery.getResultList();
+    }
+
+    @Override
+    public Optional<Employee> findEmployeeByID(Integer empID) {
+        // get the current hibernate session
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        // find the Employee by ID and return it.
+        Employee foundEmpInDB = currentSession.find(Employee.class, empID);
+
+        return Optional.of(foundEmpInDB);
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        // get the current hibernate session
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        // save the Employee
+        currentSession.saveOrUpdate(employee);
+
+    }
+
+    @Override
+    public void deleteEmployeeByID(Integer empID) {
+        // get the current hibernate session
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        // find the Employee in the DB
+        Employee foundEmpInDB = currentSession.find(Employee.class,empID);
+
+        // delete if found
+        if (foundEmpInDB != null)
+            currentSession.delete(foundEmpInDB);
     }
 }
